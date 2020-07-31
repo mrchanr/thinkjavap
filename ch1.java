f356d5ba2b820662a0a791cbcaf5f726108c9a68
mrchanr <42151767+mrchanr@users.noreply.github.com>
2020-07-31 14:46:29 -0700

    Add ch.1  excercises

---
+++ ch1.java
@@ -0,0 +1,8 @@
+public class ch1 {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");  // first line
+        System.out.println("How are you?");   // another line
+    }
+}
